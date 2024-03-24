9557a8f454bffe0d5b6df82ec25e7546b139b5c4
eduardo cerino <cerinosastrej@gmail.com>
2024-03-24 13:59:45 -0600

    error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,7 @@
-"use client"
 import { Inter } from "next/font/google";
 import "./globals.css";
-import PrelineScript from "./components/PrelineScript";
-import { useEffect } from "react";
-import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
-
-
-
 
 
 export default function RootLayout({ children }) {
@@ -17,9 +10,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <Script
-        src="https://cdn.jsdelivr.net/npm/preline@2.0.0/dist/preline.min.js"
-        strategy="lazyOnload"
-      />    </html>
+    </html>
   );
 }
